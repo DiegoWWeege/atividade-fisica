6610d2d4503c516dd20139ff64b0e17471837e6f
Arthur Pagliarini Martins <arthur_p_martins@estudante.sesisenai.org.br>
2026-08-07 21:42:45 -0300

    Deixando bem top

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-function calculaTempoPercurso(){
-    let distancia = document.getElementById("distancia").value;
-    let velocidade = document.getElementById("velocidade").value;
-    let tempo = distancia / velocidade;
-    document.getElementById("tempo-percurso").innerHTML =  tempo + " horas";
-}
-
-function calculaQuantVezes(){
-    let distancia = document.getElementById("distancia").value;
-    let tempo = distancia / 2;
-    document.getElementById("quant-vezes").innerHTML = tempo + " vezes";
-}
-
-function calculaQuantCalorias(){
-    let distancia = document.getElementById("distancia").value;
-    let velocidade = document.getElementById("velocidade").value;
-    let tempo = distancia / velocidade;
-    let caloriasGastas = tempo * 300;
-    document.getElementById("quant-calorias").innerHTML = caloriasGastas + " Calorias";
-}
-
-
-
-
+function calcular(){
+    let d=Number(document.getElementById("distancia").value);
+    let t=Number(document.getElementById("tempo").value);
+    
+    if(d<=0||t<=0)return alert("Informe valores válidos.");
+    
+    document.getElementById("velocidade").textContent=(d/(t/60)).toFixed(2);
+    document.getElementById("resultadoTempo").textContent=t;
+    document.getElementById("resultadoDistancia").textContent=(d*1000).toFixed(0);
+    document.getElementById("ritmo").textContent=(t/d).toFixed(2);
+    }
+    
